bc63cb8d9e7a284af64d94a4eb8f5095efb750c2
edwinchukus <edwin.nwofor@yahoo.com>
2014-10-01 14:26:03 +0100

    I am practicing loop

--- tutorial.one/practice.two.rb
+++ tutorial.one/practice.two.rb
@@ -1,18 +1,22 @@
 class Fruits
   def apple(*name)
     @fruitname = *name
-    puts @fruitname
-    
+    2.times do puts @fruitname
+      #self.mangoes(@country_of_origin)
+    end    
   end
   
-  def manges(*location)
-    puts @fruitname
-    self.apple(@fruitname)
+  def mangoes(*location)
+    0.times do puts @fruitname
+    end
+    
     @country_of_origin = *location
-    puts @country_of_origin
+    2.times do puts @country_of_origin
+    end
+    self.apple(@fruitname)
   end
 end
 
 calling = Fruits.new
 calling.apple("Red Apple", "Green Apple", "Pink Lady")
-calling.manges("Nigeria", "United Kingdom", "Israel")
+calling.mangoes("Nigeria", "United Kingdom", "Israel")
