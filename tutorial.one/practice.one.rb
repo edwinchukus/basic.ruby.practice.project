a6bab9da4ffc1416c06e2a9190f1a41baa01e412
edwinchukus <edwin.nwofor@yahoo.com>
2014-10-01 19:28:14 +0100

    Updated

--- tutorial.one/practice.one.rb
+++ tutorial.one/practice.one.rb
@@ -100,3 +100,31 @@ calling = Travel.new
 calling.outbound_journey("Edwin Nwofor", "London")
 calling.inbound_journey("Ijeoma Nwofor", "Amsterdern")
 
+
+
+
+
+
+
+class Fruits
+  def apple(*name)
+    @fruitname = *name
+    2.times do puts @fruitname
+      #self.mangoes(@country_of_origin)
+    end    
+  end
+  
+  def mangoes(*location)
+    0.times do puts @fruitname
+    end
+    
+    @country_of_origin = *location
+    2.times do puts @country_of_origin
+    end
+    self.apple(@fruitname)
+  end
+end
+
+calling = Fruits.new
+calling.apple("Red Apple", "Green Apple", "Pink Lady")
+calling.mangoes("Nigeria", "United Kingdom", "Israel")
--- tutorial.one/practice.two.rb
+++ tutorial.one/practice.two.rb
@@ -1,22 +1,3 @@
-class Fruits
-  def apple(*name)
-    @fruitname = *name
-    2.times do puts @fruitname
-      #self.mangoes(@country_of_origin)
-    end    
-  end
-  
-  def mangoes(*location)
-    0.times do puts @fruitname
-    end
-    
-    @country_of_origin = *location
-    2.times do puts @country_of_origin
-    end
-    self.apple(@fruitname)
-  end
-end
-
-calling = Fruits.new
-calling.apple("Red Apple", "Green Apple", "Pink Lady")
-calling.mangoes("Nigeria", "United Kingdom", "Israel")
+timing = Time.new 
+a = timing.day
+puts a 
