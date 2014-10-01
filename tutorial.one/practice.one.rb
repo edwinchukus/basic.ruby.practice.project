5f4b1323f572e439f55a461db98f07753124a487
edwinchukus <edwin.nwofor@yahoo.com>
2014-10-01 09:23:10 +0100

    pratcice.one code

--- tutorial.one/practice.one.rb
+++ tutorial.one/practice.one.rb
@@ -26,3 +26,17 @@ calling = Testing.new
 
 calling.talk
 calling.smile
+
+
+
+
+class Culture
+def educating(name = "Edwin")
+  puts "My name is "+ name
+  puts "My intent is to be educated in Ruby language!"
+end
+end
+
+calling = Culture.new
+calling.educating
+
